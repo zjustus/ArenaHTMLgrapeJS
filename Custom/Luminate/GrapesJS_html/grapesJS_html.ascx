6b7f1edd5084aaefe24814c01939bb94fa1ce2ec
Zach Justus <zach.justus1@gmail.com>
2019-09-13 09:20:40 -0700

    cut out old code

--- Custom/Luminate/GrapesJS_html/grapesJS_html.ascx
+++ Custom/Luminate/GrapesJS_html/grapesJS_html.ascx
@@ -1,1 +1,9 @@
 <%@ Control Language="c#" Inherits="ArenaWeb.WebControls.custom.Luminate.grapesJS_html" CodeFile="grapseJS_html.ascx.cs" %>
+<asp:Panel ID="pnlEditImage" Runat="server" CssClass="editImage" Visible="False"><asp:ImageButton ID="ibEdit" ImageUrl="~Images/edit.gif" Runat="server" CausesValidation="False"></asp:ImageButton></asp:Panel>
+<asp:PlaceHolder id="HtmlHolder" runat="server"></asp:PlaceHolder>
+<asp:Panel ID="pnlEdit" Runat="server" CssClass="editWrap" Visible="False">
+    <Arena:KeepAlive ID="keepMeAlive" runat="server" />
+    <!-- GrapesJS here? -->
+    <asp:Button ID="btnSave" runat="server" CssClass="smallText" OnClick="btnSave_Click" Text="Save" CausesValidation="false" />
+    <asp:Button ID="btnCancel" runat="server" CssClass="smallText" OnClick="btnCancel_Click" Text="Cancel"  CausesValidation="false"/>
+</asp:Panel>
--- Custom/Luminate/GrapesJS_html/grapseJS_html.ascx.cs
+++ Custom/Luminate/GrapesJS_html/grapseJS_html.ascx.cs
@@ -1,4 +1,4 @@
-namespace ArenaWeb.WebControls
+namespace ArenaWeb.WebControls.custom.Luminate
 {
 	using System;
 	using System.Web.UI;
@@ -10,7 +10,7 @@ namespace ArenaWeb.WebControls
 	using Arena.Portal;
 	using Arena.Security;
 	using Arena.Utility;
-	using Telerik.Web.UI;
+	//using Telerik.Web.UI; //removed
 
 	/// <summary>
 	///		Summary description for HtmlText.
@@ -24,9 +24,6 @@ namespace ArenaWeb.WebControls
         private string documentPaths = string.Empty;
 
         #region Module Settings
-
-        [CustomListSetting("Scheme", "The visual scheme that should be loaded. If not provided then path in the web.config used.", false, "", new string[] { "Default", "Black", "Forest", "Hay", "Office2007", "Office2010Black", "Office2010Blue", "Office2010Silver", "Outlook", "Simple", "Sitefinity", "Sunset", "Telerik", "Transparent", "Vista", "Web20", "WebBlue", "Windows7" }, new string[] { "Default", "Black", "Forest", "Hay", "Office2007", "Office2010Black", "Office2010Blue", "Office2010Silver", "Outlook", "Simple", "Sitefinity", "Sunset", "Telerik", "Transparent", "Vista", "Web20", "WebBlue", "Windows7" })]
-        public string SchemeSetting { get { return Setting("Scheme", string.Empty, false); } }
 
 		[MultiTenantIgnore]
         [TextSetting("Image Paths", "Comma delimited list of image directories. If not provided then path in the web.config used.", false)]
@@ -44,32 +41,11 @@ namespace ArenaWeb.WebControls
         [TextSetting("Document Paths", "Comma delimited list of document directories. If not provided then path in the web.config used.", false)]
         public string DocumentPathsSetting { get { return Setting("DocumentPaths", string.Empty, false); } }
 
-        [BooleanSetting("Clear Format on Paste", "Flag indicating whether all formatting should be removed when pasting.", false, true)]
-        public string ClearPasteFormattingSetting { get { return Setting("ClearPasteFormatting", "true", false); } }
-
-        [BooleanSetting("ConvertToXhtml", "Flag indicating whether to render text as Xhtml.", false, true)]
-        public string ConvertToXhtmlSetting { get { return Setting("ConvertToXhtml", "true", false); } }
-
-        [CustomListSetting("CSS Support Level", "Determines what CSS classes will be loaded into the CSS dropdown (Page/File).  <p>Page - will load all CSS classes available on the page into the drop down.<br>File - will load all of the CSS classes defined in the files provided in the CssFiles module setting. <p> If not provided then value in the web.config used. ", false, "", new string[] { "Page", "File" }, new string[] { "Page", "File" })]
-        public string CssSupportLevelSetting { get { return Setting("CssSupportLevel", string.Empty, false); } }
-
-        [TextSetting("CSS Files", "If the CSS Support Level is set to 'File' this setting provides a list of CSS files to load into the editors CSS dropdown (files separated by ','). If not provided then path in the web.config used.", false)]
-        public string CssFilesSetting { get { return Setting("CssFiles", string.Empty, false); } }
-
         [TextSetting("Notify", "List of e-mail addresses to notify when content is changed (multiple e-mails can be provided when separated by a semi-colon.)", false)]
         public string NotifySetting { get { return Setting("Notify", string.Empty, false); } }
 
-        [TextSetting("Tools File", "Path to the editors tool file which stores the tool bar layout (ex. '~/RadControls/Editor/ToolsFile.xml'). If not provided then path in the web.config used.", false)]
-        public string ToolsFileSetting { get { return Setting("ToolsFile", string.Empty, false); } }
-
         [BooleanSetting("Evaluate Query String", "Flag indicating if occurrences of ##[QUERYSTRING_PARAMETER]## should be replaced with the value of the specified Query String parameter.", false, false)]
         public string EvaluateQueryStringSetting { get { return Setting("EvaluateQueryString", "false", false); } }
-
-        [BooleanSetting("Context Aware", "Flag indicating if content should be varied based on a context identifier provided by the page or another module.", false, false)]
-        public string ContextAwareSetting { get { return Setting("ContextAware", "false", false); } }
-
-        [TextSetting("Context Filter", "Filter to further qualify content that is based on the same context identifier.", false)]
-        public string ContextFilterSetting { get { return Setting("ContextFilter", string.Empty, false); } }
 
         [NumericSetting("Editor Width", "Optional value for the width of the text editor.  Default = 580px", false)]
         public string EditorWidthSetting { get { return Setting("EditorWidth", "580", false); } }
@@ -80,8 +56,6 @@ namespace ArenaWeb.WebControls
         [BooleanSetting("Enable Editing", "Flag indicating if editor pencil should be displayed, which allows in place editing.", false, true)]
         public string EnableEditingSetting { get { return Setting("EnableEditing", "true", false); } }
 
-		[CustomListSetting("HTML Editor New Line Break", "The type of html element that the html editor should use to create line breaks.", false, "2", typeof(EditorNewLineModes))]
-		public int HtmlEditorLineBreakSetting { get { return Convert.ToInt32(Setting("HtmlEditorLineBreak", "2", false)); } }
 
 		#endregion
 
@@ -92,16 +66,12 @@ namespace ArenaWeb.WebControls
         {
             editEnabled = CurrentModule.Permissions.Allowed(OperationType.Edit, CurrentUser) && EnableEditingSetting.Equals("true");
 
-            ContentContext contentContext = ArenaContext.Current.ContentContext;
-            if (Boolean.Parse(ContextAwareSetting) && contentContext != null)
-                htmlSource = new Html(contentContext.Guid, ContextFilterSetting).HtmlContent.Trim();
-            else
-                htmlSource = CurrentModule.Details.Trim();
+            htmlSource = CurrentModule.Details.Trim();
 
             ShowView();
         }
 
-        private void ShowView()
+        private void ShowView() //no change
         {
             pnlEditImage.Visible = editEnabled;
             ibEdit.Visible = editEnabled;
@@ -138,15 +108,8 @@ namespace ArenaWeb.WebControls
             HtmlHolder.Visible = false;
             pnlEdit.Visible = true;
 
-            //************************
-
-            Utilities.SetupRadEditor(radEditor, (string.IsNullOrEmpty(SchemeSetting)?string.Empty:SchemeSetting),
-                ToolsFileSetting, CssFilesSetting, ImagesPathsSetting,
-                DocumentPathsSetting, MediaPathsSetting, FlashPathsSetting,
-                Boolean.Parse(ConvertToXhtmlSetting),
-                EditorHeightSetting, EditorWidthSetting, (EditorNewLineModes)HtmlEditorLineBreakSetting);
-
-            radEditor.Content = Server.HtmlDecode(htmlSource);
+			//edditor here?
+            //radEditor.Content = Server.HtmlDecode(htmlSource); //this calles the htmlSource code
 
         }
 
@@ -173,24 +136,12 @@ namespace ArenaWeb.WebControls
 
         protected void btnSave_Click(object sender, System.EventArgs e)
         {
-            ContentContext contentContext = ArenaContext.Current.ContentContext;
-            if (Boolean.Parse(ContextAwareSetting) && contentContext != null)
-            {
-                Html html = new Html(contentContext.Guid, ContextFilterSetting);
-                html.ContextTable = contentContext.SourceTable;
-                html.ContextGuid = contentContext.Guid;
-                html.ContextFilter = ContextFilterSetting;
-                html.HtmlContent = Server.HtmlEncode(radEditor.Content);
-                html.Save(CurrentUser.Identity.Name);
-                htmlSource = html.HtmlContent.Trim();
-            }
-            else
-            {
-                CurrentModule.Details = Server.HtmlEncode(radEditor.Content);
-                CurrentModule.Save(CurrentUser.Identity.Name);
-                htmlSource = CurrentModule.Details.Trim();
-            }
 
+			/*
+			CurrentModule.Details = Server.HtmlEncode(radEditor.Content); //this part saves the HTML to
+			CurrentModule.Save(CurrentUser.Identity.Name);
+			htmlSource = CurrentModule.Details.Trim();
+			*/
             ShowView();
 
             try
